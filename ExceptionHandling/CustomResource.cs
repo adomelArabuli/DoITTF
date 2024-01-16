0f53fb4110dca30d5ab364999d7663a01ccd6b5d
a.arabuli <a.arabuli@AARABULI-LP>
2024-01-16 19:37:47 +0400

    Exceptionhandling

---
+++ ExceptionHandling/CustomResource.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExceptionHandling
+{
+    internal class CustomResource : IDisposable
+    {
+        private bool isResourceOpen = false;
+
+        public void OpenResource()
+        {
+            Console.WriteLine("Resource opened");
+            isResourceOpen = true;
+        }
+
+        public void UseResource()
+        {
+            if(!isResourceOpen)
+            {
+                throw new InvalidOperationException("Resource is not open");
+            }
+
+            Console.WriteLine("Resource is being used");
+        }
+
+        public void CloseResource()
+        {
+            Console.WriteLine("Resource closed");
+            isResourceOpen = false;
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                if (isResourceOpen)
+                {
+                    CloseResource();
+                }
+            }
+        }
+    }
+}
---
+++ ExceptionHandling/Program.cs
@@ -0,0 +1,109 @@
+﻿using System.Text;
+using System.Threading.Channels;
+
+namespace ExceptionHandling
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Exceptions
+            using (CustomResource cr = new CustomResource())
+            {
+                cr.OpenResource();
+                cr.UseResource();
+            }
+
+            #region FileStream
+            //FileStream fileStream = null;
+            //try
+            //{
+            //    fileStream = new FileStream("C:\\Users\\a.arabuli\\Desktop\\Create.txt", FileMode.Create);
+
+            //    byte[] buffer = Encoding.ASCII.GetBytes("Hello world");
+
+            //    fileStream.Write(buffer, 0, buffer.Length);
+
+            //    throw new ApplicationException("Simulated exception");
+
+            //}
+            //catch (Exception ex)
+            //{
+            //    Console.WriteLine("Test");
+            //}
+            //finally
+            //{
+            //    if (fileStream != null)
+            //    {
+            //        fileStream.Close();
+            //        Console.WriteLine("Filestream was closed");
+            //    }
+            //}
+            //
+            #endregion
+
+            #region comment
+
+            //string path = "C:\\Users\\a.arabuli\\Desktop\\roadmap1.txt";
+
+            //int a = 5;
+            //Console.WriteLine(a);
+            //ChangeValue(a);
+            //Console.WriteLine(a);
+
+            //try
+            //{
+            //    //int result = DevideByZero(10, 5);
+
+            //    //int userInput = ParseInt("Abc");
+
+            //    //ReadFile(path);
+            //}
+            ////catch (DivideByZeroException ex)
+            ////{
+            ////    Console.WriteLine("Devide by zero is forbidden");
+            ////}
+            ////catch (FormatException ex)
+            ////{
+            ////    Console.WriteLine("Could not format");
+            ////}
+            ////catch (FileNotFoundException ex)
+            ////{
+            ////    Console.WriteLine("File not found");
+            ////}
+            //catch (Exception ex)
+            //{
+            //    Console.WriteLine("General exception" + " " + ex.Message);
+            //}
+            //finally
+            //{
+            //    Console.WriteLine("Finally executed");
+            //}
+            #endregion
+        }
+
+        static void ChangeValue(int a)
+        {
+            a++;
+        }
+
+        static int DevideByZero(int value1, int value2)
+        {
+            return value1 / value2;
+        }
+
+        static int ParseInt(string input)
+        {
+            return int.Parse(input);
+        }
+
+        static void ReadFile(string path)
+        {
+            using (StreamReader sr = new StreamReader(path))
+            {
+                string content = sr.ReadToEnd();
+                Console.WriteLine($"The content of file is: {content}");
+            }
+        }
+    }
+}
