684fb7d36a28e5d1f5979015450ecf6a815e59f0
a.arabuli <a.arabuli@AARABULI-LP>
2024-02-02 18:27:17 +0400

    Events part2

--- Events/Program.cs
+++ Events/Program.cs
@@ -6,66 +6,112 @@ namespace Events
     {
         static void Main(string[] args)
         {
-            YoutubeChannel youtuber = new YoutubeChannel();
-            Subscriber subscriber = new Subscriber();
+            //YoutubeChannel youtuber = new YoutubeChannel();
+            //Subscriber subscriber = new Subscriber();
 
-            subscriber.Subscribe(youtuber);
+            //subscriber.Subscribe(youtuber);
 
-            youtuber.UploadVideo();
+            //youtuber.UploadVideo();
 
-            //subscriber.UnSubscribe(youtuber);
+            ////subscriber.UnSubscribe(youtuber);
 
-            // dagvrcha(using event for multicast delegate)
-            youtuber.UploadVideo();
+            //// dagvrcha(using event for multicast delegate)
+            //youtuber.UploadVideo();
 
-            // publisher(party- event: OnCakeCut), subscribers(guest) 
+            EventPublisher publisher = new EventPublisher();
+            EventSubscriber subscriber = new EventSubscriber();
+
+            subscriber.Subscribe(publisher);
+
+            publisher.TriggerEvent();
         }
     }
+
+    class EventSubscriber
+    {
+        private void MyEventHandler1(string message)
+        {
+            Console.WriteLine($"Handler 1 {message}");
+        }
+
+        private void MyEventHandler2(string message)
+        {
+            Console.WriteLine($"Handler 2 {message}");
+        }
+
+        public void Subscribe(EventPublisher publisher)
+        {
+            publisher.MyEvent += MyEventHandler1;
+            publisher.MyEvent += MyEventHandler2;
+        }
+
+        public void UnSubscribe(EventPublisher publisher)
+        {
+            publisher.MyEvent -= MyEventHandler1;
+            publisher.MyEvent -= MyEventHandler2;
+        }
+    }
+
+    public class EventPublisher
+    {
+        public event Action<string> MyEvent;
+
+        protected virtual void OnMyEvent(string message)
+        {
+            MyEvent(message); // Invoke ? (is better? why?)
+        } 
+
+        public void TriggerEvent()
+        {
+            OnMyEvent("Event triggered");
+        }
+    }
+
 
     //Publisher
 
-    public class MyEventArgs : EventArgs 
-    {
-        public string Message { get;}
-        public MyEventArgs(string message)
-        {
-            Message = message;
-        }
-    }
+    //public class MyEventArgs : EventArgs 
+    //{
+    //    public string Message { get;}
+    //    public MyEventArgs(string message)
+    //    {
+    //        Message = message;
+    //    }
+    //}
 
-    public class YoutubeChannel
-    {
-        public event EventHandler<MyEventArgs> VideoUploadEvent;
+    //public class YoutubeChannel
+    //{
+    //    public event EventHandler<MyEventArgs> VideoUploadEvent;
 
-        protected virtual void OnVideoUpload(string message)
-        {
-            MyEventArgs args = new MyEventArgs(message);
-            VideoUploadEvent?.Invoke(this, args);
-        }
+    //    protected virtual void OnVideoUpload(string message)
+    //    {
+    //        MyEventArgs args = new MyEventArgs(message);
+    //        VideoUploadEvent?.Invoke(this, args);
+    //    }
 
-        public void UploadVideo()
-        {
-            string path = "https://Youtube.com/channel1/newVideo";
-            OnVideoUpload($"Check video here: {path}");
-        }
-    }
+    //    public void UploadVideo()
+    //    {
+    //        string path = "https://Youtube.com/channel1/newVideo";
+    //        OnVideoUpload($"Check video here: {path}");
+    //    }
+    //}
 
-    //Subscriber
-    public class Subscriber
-    {
-        private void VideoUploadEventHandler(object sender, MyEventArgs e)
-        {
-            Console.WriteLine($"Alert! new video was uploaded {e.Message}");
-        }
+    ////Subscriber
+    //public class Subscriber
+    //{
+    //    private void VideoUploadEventHandler(object sender, MyEventArgs e)
+    //    {
+    //        Console.WriteLine($"Alert! new video was uploaded {e.Message}");
+    //    }
 
-        public void Subscribe(YoutubeChannel channel)
-        {
-            channel.VideoUploadEvent += VideoUploadEventHandler;
-        }
+    //    public void Subscribe(YoutubeChannel channel)
+    //    {
+    //        channel.VideoUploadEvent += VideoUploadEventHandler;
+    //    }
 
-        public void UnSubscribe(YoutubeChannel channel)
-        {
-            channel.VideoUploadEvent -= VideoUploadEventHandler;
-        }
-    }
+    //    public void UnSubscribe(YoutubeChannel channel)
+    //    {
+    //        channel.VideoUploadEvent -= VideoUploadEventHandler;
+    //    }
+    //}
 }
