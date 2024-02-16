546c74bf9e61da73f3cf1b3c74c67b81229651b6
a.arabuli <a.arabuli@AARABULI-LP>
2024-02-16 19:38:31 +0400

    test

---
+++ FileDirectoryFileInfoDirectoryInfo/Program.cs
@@ -0,0 +1,63 @@
+﻿using System.Globalization;
+using System.Net.Http.Headers;
+using System.Reflection.Metadata;
+using System.Text;
+
+namespace FileDirectoryFileInfoDirectoryInfo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // file - Copy, Create, Move, Delete, Open, Encrypt, Decrypt.
+            //DirectoryInfo directoryInfo = new DirectoryInfo("C:\\Users\\a.arabuli\\Desktop");
+
+            //FileInfo newFile = new FileInfo(Path.Combine(directoryInfo.FullName, "NewFile.txt"));
+
+            //if(!newFile.Exists)
+            //{
+            //    using(StreamWriter sw = newFile.CreateText())
+            //    {
+            //        sw.WriteLine("Hello");
+            //    }
+            //    Console.WriteLine("Success");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("File already exists");
+            //}
+
+            //FileInfo fileInfo = new FileInfo("C:\\Users\\a.arabuli\\Desktop\\NewFile.txt");
+            //Console.WriteLine($"File info name: {fileInfo.Name}");
+            //Console.WriteLine($"File info full name: {fileInfo.FullName}");
+            //Console.WriteLine($"File extension: {fileInfo.Extension}");
+            //Console.WriteLine($"File size: {fileInfo.Length} in bytes");
+
+            //string content = File.ReadAllText(fileInfo.FullName);
+            //Console.WriteLine($"File content {content}");
+
+            //Console.InputEncoding = System.Text.Encoding.Unicode;
+            //Console.OutputEncoding = System.Text.Encoding.Unicode;
+            //CultureInfo test = new CultureInfo("ka-GE");
+            //Console.OutputEncoding = System.Text.Encoding.GetEncoding("Cyrillic");
+            //Console.OutputEncoding = Console.InputEncoding = Encoding.Unicode;
+            //var test = Console.ReadLine();
+            //Console.WriteLine("ტესტ");
+            //DirectoryInfo directoryInfo = new DirectoryInfo("C:\\Users\\a.arabuli\\Desktop");
+
+            //FileInfo[] files = directoryInfo.GetFiles();
+
+            //foreach (FileInfo file in files)
+            //{
+            //    Console.WriteLine(file.Name);
+            //}
+            //DirectoryInfo parentDirectory = new DirectoryInfo("C:\\Users\\a.arabuli\\Desktop\\ITStep\\TestDirectory");
+            //DirectoryInfo newDirectory = parentDirectory.CreateSubdirectory("TestDirectory");
+            //DirectoryInfo directoryToDelete = new DirectoryInfo("C:\\Users\\a.arabuli\\Desktop\\ITStep\\Test");
+            //if(directoryToDelete.Exists )
+            //{
+            //    directoryToDelete.Delete();
+            //}
+        }
+    }
+}
